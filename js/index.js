f3a00f11f6f36826e99968ddccfe23dad50f7ade
Wendy <wendydeluca96@hotmail.com>
2023-12-07 00:01:36 +0100

    changes applied

--- js/index.js
+++ js/index.js
@@ -1,16 +1,15 @@
 // 1) creo il mio tag input che all'avvio deve presentare un Inputfield
-
 const inputField = function () {
   const input = document.createElement("input");
   input.setAttribute("type", "text"); // inserisco gli attributi del tag input
-  input.setAttribute("placeholder", "Inserisci nome del task"); // inserisco gli attributi del tag input
+  input.setAttribute("placeholder", "Make a wish.."); // inserisco gli attributi del tag input
 
   //   2) richiamo il tag parent per appenderci dentro il figlio
   const myDiv = document.getElementById("first-div"); // richiamo il div con il suo ID
   myDiv.appendChild(input); // appendo il child (tag input) nel suo parent (div)
 
   const button = document.createElement("button"); // creo un bottone
-  button.innerText = "AGGIUNGI";
+  button.innerText = "INVIO";
 
   const ul = document.createElement("ul"); // creo la mia unordered list
   myDiv.appendChild(ul); // inserisco la ul nel parent tag
@@ -18,13 +17,14 @@ const inputField = function () {
   myDiv.appendChild(button);
   button.addEventListener("click", function (e) {
     // associo un comportamento al button
+
     const li = document.createElement("li"); // creo l'elemento della ul
     ul.appendChild(li);
     li.innerText = input.value;
     input.value = ""; //  reset
     const deletebtn = document.createElement("button"); // il mio button delete individuale
     li.appendChild(deletebtn);
-    deletebtn.innerText = "rimuovi";
+    deletebtn.innerText = "x";
     deletebtn.addEventListener("click", function (e) {
       li.remove();
     });
